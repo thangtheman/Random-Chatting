80b4412cecf934bd54fe46ba19e46c5ca033bb2b
thangtheman <thang@fromttm.com>
2015-05-19 11:07:37 -0700

    make it better

--- routes.js
+++ routes.js
@@ -54,7 +54,7 @@ module.exports = function(app,io){
 					id: data
 				});
 			}
-			else if(room.length >= 2) {
+			else if(room.length >= 10) {
 
 				chat.emit('tooMany', {boolean: true});
 			}
@@ -66,7 +66,7 @@ module.exports = function(app,io){
 
 			var room = findClientsSocket(io, data.id);
 			// Only two people per room are allowed
-			if (room.length < 2) {
+			if (room.length < 10) {
 
 				// Use the socket object to store data. Each client gets
 				// their own unique socket object
